fcb4e7a213c7990161af89071d0e6083dd3da336
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2024-08-26 21:03:34 +0300

    optimize chat

---
+++ src/main/java/gg/darkaddons/mixins/MixinDSMLootTracker.java
@@ -0,0 +1,22 @@
+package gg.darkaddons.mixins;
+
+import org.jetbrains.annotations.NotNull;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Pseudo
+@Mixin(targets = "me.Danker.features.loot.LootTracker", remap = false, priority = 1_001)
+final class MixinDSMLootTracker {
+    private MixinDSMLootTracker() {
+        super();
+    }
+
+    @Inject(method = "onChat", remap = false, at = @At("HEAD"), cancellable = true)
+    private final void onChat$darkaddons(@NotNull final CallbackInfo ci) {
+        ci.cancel();
+    }
+}
