54810a2826d4e1aa6dc8a16f3a260cc6f09bc2e8
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2024-09-24 19:09:54 +0300

    remove > replace

--- src/main/java/gg/darkaddons/mixins/MixinCapeManager.java
+++ src/main/java/gg/darkaddons/mixins/MixinCapeManager.java
@@ -1,11 +1,17 @@
 package gg.darkaddons.mixins;
 
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.UUID;
+
+import net.minecraft.client.Minecraft;
 
 @Pseudo
 @Mixin(targets = "io.github.moulberry.notenoughupdates.cosmetics.CapeManager", priority = 999)
@@ -14,27 +20,44 @@ final class MixinCapeManager {
         super();
     }
 
+    @Unique
+    @Nullable
+    private static String selfUUIDCached;
+
+    @Redirect(method = "onPlayerTick", at = @At(value = "INVOKE", target = "Ljava/util/UUID;toString()Ljava/lang/String;"), remap = false)
+    @NotNull
+    private final String toString$darkaddons(@NotNull final UUID uuid) {
+        final var selfUUID = Minecraft.getMinecraft().thePlayer.getUniqueID();
+        final var self = selfUUID == uuid;
+
+        if (self && null == MixinCapeManager.selfUUIDCached) {
+            MixinCapeManager.selfUUIDCached = StringUtils.remove(selfUUID.toString(), '-');
+        }
+
+        return self ? selfUUIDCached : uuid.toString();
+    }
+
     @Redirect(method = "onPlayerTick", at = @At(value = "INVOKE", target = "Ljava/lang/String;replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;"), remap = false)
     @NotNull
     private final String replace$darkaddons(@NotNull final String text, @NotNull final CharSequence search, @NotNull final CharSequence replacement) {
-        return StringUtils.replace(text, search.toString(), replacement.toString());
+        return StringUtils.remove(text, '-');
     }
 
     @Redirect(method = "onRenderPlayer", at = @At(value = "INVOKE", target = "Ljava/lang/String;replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;"), remap = false)
     @NotNull
     private final String replace$darkaddons$2(@NotNull final String text, @NotNull final CharSequence search, @NotNull final CharSequence replacement) {
-        return StringUtils.replace(text, search.toString(), replacement.toString());
+        return StringUtils.remove(text, '-');
     }
 
     @Redirect(method = "setCape", at = @At(value = "INVOKE", target = "Ljava/lang/String;replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;"), remap = false)
     @NotNull
     private final String replace$darkaddons$3(@NotNull final String text, @NotNull final CharSequence search, @NotNull final CharSequence replacement) {
-        return StringUtils.replace(text, search.toString(), replacement.toString());
+        return StringUtils.remove(text, '-');
     }
 
     @Redirect(method = "updateCapes", at = @At(value = "INVOKE", target = "Ljava/lang/String;replace(Ljava/lang/CharSequence;Ljava/lang/CharSequence;)Ljava/lang/String;"), remap = false)
     @NotNull
     private final String replace$darkaddons$4(@NotNull final String text, @NotNull final CharSequence search, @NotNull final CharSequence replacement) {
-        return StringUtils.replace(text, search.toString(), replacement.toString());
+        return StringUtils.remove(text, '-');
     }
 }
