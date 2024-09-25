9e26bfef43ac89b14680fcad078bfa7b6b13104f
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2024-09-25 15:28:55 +0300

    optimize layerarmorbase further

--- src/main/java/gg/darkaddons/mixins/MixinLayerArmorBase.java
+++ src/main/java/gg/darkaddons/mixins/MixinLayerArmorBase.java
@@ -17,12 +17,11 @@ final class MixinLayerArmorBase {
     @NotNull
     private final String stringFormat$darkaddons(@NotNull final String text, @NotNull final Object... values) {
         final var len = values.length;
+
         return switch (len) {
-            case 1 -> StringUtils.replaceOnce(text, "%s", values[0].toString());
-            case 4 ->
-                StringUtils.replaceOnce(StringUtils.replaceOnce(StringUtils.replaceOnce(StringUtils.replaceOnce(text, "%s", values[0].toString()), "%s", values[1].toString()), "%d", values[2].toString()), "%s", values[3].toString());
-            case 3 ->
-                StringUtils.replaceOnce(StringUtils.replaceOnce(StringUtils.replaceOnce(text, "%s", values[0].toString()), "%d", values[1].toString()), "%s", values[2].toString());
+            case 4 -> values[0].toString() + ":textures/models/armor/" + values[1].toString() + "_layer_" + values[2].toString() + values[3].toString() + ".png";
+            case 1 -> '_' + values[0].toString();
+            case 3 -> "textures/models/armor/" + values[0].toString() + "_layer_" + values[1].toString() + values[2].toString() + ".png";
             default -> throw new IllegalStateException("unexpected len of " + len);
         };
     }
