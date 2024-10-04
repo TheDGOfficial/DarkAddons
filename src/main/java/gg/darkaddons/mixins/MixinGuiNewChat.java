07ab73e9f4960dda794d97df16cd1c3adadbbfc3
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2024-10-04 08:10:31 +0300

    fix sentmessages memory leak

---
+++ src/main/java/gg/darkaddons/mixins/MixinGuiNewChat.java
@@ -0,0 +1,37 @@
+package gg.darkaddons.mixins;
+
+import gg.darkaddons.mixin.MixinUtils;
+
+import net.minecraft.client.gui.GuiNewChat;
+
+import org.jetbrains.annotations.NotNull;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(value = GuiNewChat.class, priority = 1_001)
+final class MixinGuiNewChat {
+    private MixinGuiNewChat() {
+        super();
+    }
+
+    @Inject(method = "addToSentMessages", at = @At(value = "TAIL"))
+    private final void afterAddToSentMessages$darkaddons(@NotNull final String message, @NotNull final CallbackInfo ci) {
+        final var sentMessages = this.getSentMessages();
+
+        while (sentMessages.size() > 100) {
+            sentMessages.remove(0);
+        }
+    }
+
+    @Shadow
+    private final List<String> getSentMessages() {
+        throw MixinUtils.shadowFail();
+    }
+}
---
+++ src/main/java/gg/darkaddons/mixins/MixinIngameChatManager.java
@@ -0,0 +1,39 @@
+package gg.darkaddons.mixins;
+
+import gg.darkaddons.mixin.MixinUtils;
+
+import net.minecraft.client.gui.GuiNewChat;
+
+import org.jetbrains.annotations.NotNull;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Pseudo
+@Mixin(targets = "net.labymod.ingamechat.IngameChatManager", remap = false, priority = 1_001)
+final class MixinIngameChatManager {
+    private MixinIngameChatManager() {
+        super();
+    }
+
+    @Inject(method = "addToSentMessages", at = @At(value = "TAIL"))
+    private final void afterAddToSentMessages$darkaddons(@NotNull final String message, @NotNull final CallbackInfo ci) {
+        final var sentMessages = this.getSentMessages();
+
+        while (sentMessages.size() > 100) {
+            sentMessages.remove(0);
+        }
+    }
+
+    @Shadow
+    private final List<String> getSentMessages() {
+        throw MixinUtils.shadowFail();
+    }
+}
