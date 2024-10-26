7c01c225eaca666800f3172e38e5129d5defff11
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2024-10-26 17:19:49 +0300

    update errorprone

--- optimize.sh
+++ optimize.sh
@@ -35,7 +35,7 @@ GSON_VERSION=2.2.4
 
 ASM_VERSION=9.7
 
-ERRORPRONE_VERSION=2.34.0
+ERRORPRONE_VERSION=2.35.1
 
 DARKADDONS_VERSION=${versionProperties["darkaddons.version"]}
 SKYTILS_VERSION=${versionProperties["skytils.version"]}
