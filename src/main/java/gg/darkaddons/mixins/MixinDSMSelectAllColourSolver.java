65d714e7d999baeaeabdd067666bd23180407b28
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2024-12-23 10:51:17 +0300

    something

---
+++ src/main/java/gg/darkaddons/mixins/MixinDSMSelectAllColourSolver.java
@@ -0,0 +1,34 @@
+package gg.darkaddons.mixins;
+
+import org.jetbrains.annotations.NotNull;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Locale;
+
+@Pseudo
+@Mixin(targets = "me.Danker.features.puzzlesolvers.SelectAllColourSolver", remap = false, priority = 1_001)
+final class MixinDSMSelectAllColourSolver {
+    private MixinDSMSelectAllColourSolver() {
+        super();
+    }
+
+    @Redirect(method = "onGuiRender", at = @At(value = "INVOKE", target = "Ljava/lang/String;toUpperCase()Ljava/lang/String;", remap = false))
+    @NotNull
+    private final String toUpperCase$darkaddons(@NotNull final String text) {
+        return text.toUpperCase(Locale.ROOT);
+    }
+
+    @Redirect(method = "onGuiRender", remap = false, at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"))
+    private final boolean onGuiRender$darkaddons(@NotNull final String itemName, @NotNull final CharSequence terminalColorNeeded) {
+        if (itemName.contains(terminalColorNeeded)) {
+            final var wantedBlue = "BLUE".equals(terminalColorNeeded);
+            return !wantedBlue || !itemName.contains("LIGHT BLUE");
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/gg/darkaddons/mixins/MixinSelectAllColorSolver.java
@@ -0,0 +1,29 @@
+package gg.darkaddons.mixins;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Pseudo
+@Mixin(targets = "gg.skytils.skytilsmod.features.impl.dungeons.solvers.terminals.SelectAllColorSolver", remap = false, priority = 1_001)
+final class MixinSelectAllColorSolver {
+    private MixinSelectAllColorSolver() {
+        super();
+    }
+
+    @Redirect(method = "onBackgroundDrawn", remap = false, at = @At(value = "INVOKE", target = "Lkotlin/text/StringsKt;contains$default(Ljava/lang/CharSequence;Ljava/lang/CharSequence;ZILjava/lang/Object;)Z", ordinal = 1))
+    private final boolean onBackgroundDrawn$darkaddons(@NotNull final CharSequence iName, @NotNull final CharSequence termColorNeeded, final boolean ignoreCase, final int i, @Nullable final Object o) {
+        final var itemName = iName.toString();
+        final var terminalColorNeeded = termColorNeeded.toString();
+        if (itemName.contains(terminalColorNeeded)) {
+            final var wantedBlue = "BLUE".equals(terminalColorNeeded);
+            return !wantedBlue || !itemName.contains("lightBlue");
+        } else {
+            return false;
+        }
+    }
+}
