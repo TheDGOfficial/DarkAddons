de12b73e9e0bf5c0e68ef5c74972be80d1f5e7c2
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-01-05 15:43:12 +0300

    fix stuff

--- src/main/java/gg/darkaddons/mixins/MixinConfigCore.java
+++ src/main/java/gg/darkaddons/mixins/MixinConfigCore.java
@@ -1,12 +1,17 @@
 package gg.darkaddons.mixins;
 
+import gg.darkaddons.mixin.MixinUtils;
+
 import gg.darkaddons.DarkAddons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -20,12 +25,20 @@ final class MixinConfigCore {
         super();
     }
 
-    @Redirect(method = "<clinit>", remap = false, at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ScheduledExecutorService;scheduleAtFixedRate(Ljava/lang/Runnable;JJLjava/util/concurrent/TimeUnit;)Ljava/util/concurrent/ScheduledFuture;", remap = false))
+    @Shadow
+    private static final void saveAll() {
+        throw MixinUtils.shadowFail();
+    }
+
+    @Inject(method = "<clinit>", remap = false, at = @At(value = "TAIL"))
+    private static final void afterStaticInitializer$darkaddons(@NotNull final CallbackInfo ci) {
+        // Save before cleanly exiting the game via "Quit Game" in the main menu in case periodic save didn't kick in after changing settings in time (or disabled)
+        DarkAddons.addShutdownTask(MixinConfigCore::saveAll);
+    }
+
+    @Redirect(method = "<clinit>", remap = false, at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ScheduledExecutorService;scheduleAtFixedRate(Ljava/lang/Runnable;JJLjava/util/concurrent/TimeUnit;)Ljava/util/concurrent/ScheduledFuture;", remap = false), require = 0)
     @Nullable
     private static final ScheduledFuture<?> scheduleAtFixedRate$darkaddons(@NotNull final ScheduledExecutorService executor, @NotNull final Runnable task, final long initialDelay, final long runInterval, @NotNull final TimeUnit timeUnit) {
-        // Save before cleanly exiting the game via "Quit Game" in the main menu in case periodic save didn't kick in after changing settings in time (or disabled)
-        DarkAddons.addShutdownTask(task);
-
         // scheduleWithFixedDelay instead of scheduleAtFixedRate: this will take time took to save the files to the config into account, so if let's say saving the config took 29.9 seconds and the runInterval * 20 = 30 seconds, it will not re-save it after 0.1 seconds again but will wait 30 seconds.
 
         // runInterval * 20: one config saves all configs to disk every 30 seconds even if its unchanged, this makes it 600 seconds (10 minutes), this should reduce I/O and increase SSD lifespan/reduce wear.
---
+++ src/main/java/gg/darkaddons/mixins/MixinNEU.java
@@ -0,0 +1,31 @@
+package gg.darkaddons.mixins;
+
+import gg.darkaddons.mixin.MixinUtils;
+
+import gg.darkaddons.DarkAddons;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Pseudo
+@Mixin(targets = "io.github.moulberry.notenoughupdates.NotEnoughUpdates", remap = false, priority = 1_001)
+final class MixinNEU {
+    private MixinNEU() {
+        super();
+    }
+
+    @Shadow
+    private final void saveConfig() {
+        throw MixinUtils.shadowFail();
+    }
+
+    @Inject(method = "<init>", remap = false, at = @At(value = "TAIL"))
+    private final void afterClassInitializer$darkaddons(@NotNull final CallbackInfo ci) {
+        // Save before cleanly exiting the game via "Quit Game" in the main menu in case periodic save didn't kick in after changing settings in time (or disabled)
+        DarkAddons.addShutdownTask(this::saveConfig);
+    }
+}
