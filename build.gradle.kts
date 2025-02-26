dae3d53f57b924291582239a57437b7b2ad88674
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-02-26 07:11:05 +0300

    update gradle & shadow, enable configuration cache
    and make processResources compatible with the configuration cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ import java.util.Properties
 
 plugins {
     //kotlin("jvm") version "2.1.20-dev-6370"
-    id("com.gradleup.shadow") version "9.0.0-beta8"
+    id("com.gradleup.shadow") version "9.0.0-beta9"
     id("gg.essential.loom") version "1.9.24"
     id("dev.architectury.architectury-pack200") version "0.1.3"
     id("com.github.ben-manes.versions") version "0.52.0"
@@ -199,12 +199,15 @@ sourceSets {
 
 tasks {
     processResources {
-        inputs.property("version", provider { project.version })
+        val versionProvider = providers.provider { project.version.toString() }
+        val descriptionProvider = providers.provider { project.description ?: "" }
+
+        inputs.property("version", versionProvider)
         inputs.property("mcversion", "1.8.9")
-        inputs.property("description", provider { project.description })
+        inputs.property("description", descriptionProvider)
 
         filesMatching("mcmod.info") {
-            expand(mapOf("version" to provider { project.version }.get(), "mcversion" to "1.8.9", "description" to provider { project.description }.get()))
+            expand(mapOf("version" to versionProvider.get(), "mcversion" to "1.8.9", "description" to descriptionProvider.get()))
         }
         dependsOn(compileJava)
     }
