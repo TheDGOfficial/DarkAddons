9a4501d87e598c9ff8b450c111ccac1fda884872
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-02-26 17:23:54 +0300

    fix build failure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,11 +16,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
-gradle.settingsEvaluated {
-    val gradleCacheDir = File(System.getProperty("user.home"), ".gradle/caches/essential-loom")
-    gradleCacheDir.asFileTree.matching {
-        exclude("mojang_versions_manifest.json")
-    }
-}
-
 rootProject.name = "DarkAddons"
