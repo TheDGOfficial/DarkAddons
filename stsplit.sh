69d86070b24cc75a8e7629543fa73abc34836df0
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-04-25 01:51:30 +0300

    fix shellcheck

--- stsplit.sh
+++ stsplit.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+set -eEuo pipefail
+trap 'CODE=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $CODE' ERR
+IFS=$'\n\t'
+
 java -cp ~/.m2/repository/org/ow2/asm/asm/9.8/asm-9.8.jar:. JarDependencyChecker.java ~/DarkAddons/build/libs/DarkAddons-v0.2.0-opt.jar gg.skytils
