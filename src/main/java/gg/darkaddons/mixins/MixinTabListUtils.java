ae69322febd80309ed9132a3f8327eb65fe20658
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-04-25 20:22:30 +0300

    add size hint to arraylist

--- src/main/java/gg/darkaddons/mixins/MixinTabListUtils.java
+++ src/main/java/gg/darkaddons/mixins/MixinTabListUtils.java
@@ -4,10 +4,12 @@ import org.jetbrains.annotations.NotNull;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import com.google.common.collect.Ordering;
 
@@ -18,9 +20,24 @@ final class MixinTabListUtils {
         super();
     }
 
+    @Unique
+    private List<?> immutableSortedCopy;
+
     @Redirect(method = "getTabList0", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Ordering;sortedCopy(Ljava/lang/Iterable;)Ljava/util/List;", remap = false), remap = false)
     @NotNull
     private final <T> List<T> sortedCopy$darkaddons(@NotNull final Ordering<T> ordering, @NotNull final Iterable<T> iterable) {
-        return ordering.immutableSortedCopy(iterable);
+        final var copy = ordering.immutableSortedCopy(iterable);
+        this.immutableSortedCopy = copy;
+
+        return copy;
+    }
+
+    @Redirect(method = "getTabList0", at = @At(value = "NEW", target = "java/util/ArrayList", remap = false), remap = false)
+    @NotNull
+    private final ArrayList<String> newArrayList$darkaddons() {
+        final var list = new ArrayList<String>(this.immutableSortedCopy.size());
+        this.immutableSortedCopy = null;
+
+        return list;
     }
 }
