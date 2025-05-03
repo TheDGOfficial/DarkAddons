7b5906bf91fe56c46cd91b5b27950a7674889fa0
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-05-03 03:30:11 +0300

    fix reliance on version in script

--- stsplit.sh
+++ stsplit.sh
@@ -3,4 +3,4 @@ set -eEuo pipefail
 trap 'CODE=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $CODE' ERR
 IFS=$'\n\t'
 
-java -cp ~/.m2/repository/org/ow2/asm/asm/9.8/asm-9.8.jar:. JarDependencyChecker.java ~/DarkAddons/build/libs/DarkAddons-v0.2.0-opt.jar gg.skytils
+java -cp ~/.m2/repository/org/ow2/asm/asm/9.8/asm-9.8.jar:. JarDependencyChecker.java ~/DarkAddons/build/libs/*-opt.jar gg.skytils
