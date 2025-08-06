056254743b44c769763c530c4441a16f5b07ea5d
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-08-06 07:22:39 +0300

    fix polysprint incompatibility

--- src/main/java/gg/darkaddons/mixins/MixinEntityPlayerSP.java
+++ src/main/java/gg/darkaddons/mixins/MixinEntityPlayerSP.java
@@ -8,13 +8,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = EntityPlayerSP.class, priority = 999)
+@Mixin(value = EntityPlayerSP.class, priority = 1001)
 final class MixinEntityPlayerSP {
     private MixinEntityPlayerSP() {
         super();
     }
 
-    @Redirect(method = "onLivingUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/settings/KeyBinding;isKeyDown()Z"))
+    @Redirect(method = "onLivingUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/settings/KeyBinding;isKeyDown()Z"), require = 0)
     private final boolean isKeyDown$darkaddons(@NotNull final KeyBinding keyBinding) {
         return DarkAddons.isAlwaysSprint() || keyBinding.isKeyDown();
     }
