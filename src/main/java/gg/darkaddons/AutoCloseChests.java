03395248b61b7fb853abc88de04fe917c92435b2
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-09-25 15:38:12 +0300

    fix auto close chests

--- src/main/java/gg/darkaddons/AutoCloseChests.java
+++ src/main/java/gg/darkaddons/AutoCloseChests.java
@@ -4,6 +4,8 @@ import org.jetbrains.annotations.NotNull;
 
 import net.minecraft.client.Minecraft;
 
+import net.minecraft.util.ChatComponentTranslation;
+
 import net.minecraft.network.Packet;
 
 import net.minecraft.network.play.server.S2DPacketOpenWindow;
@@ -24,10 +26,17 @@ final class AutoCloseChests {
 
     static final boolean handlePacket(@NotNull final Packet<?> packet) {
         if (Config.isAutoCloseChests() && DarkAddons.isInDungeons() && packet instanceof final S2DPacketOpenWindow packetOpenWindow) {
-            final var windowName = packetOpenWindow.getWindowTitle().getUnformattedText();
-            if ("Chest".equals(windowName) || "Large Chest".equals(windowName)) {
-                Minecraft.getMinecraft().getNetHandler().getNetworkManager().sendPacket(new C0DPacketCloseWindow(packetOpenWindow.getWindowId()));
-                return false;
+            final var title = packetOpenWindow.getWindowTitle();
+            final var slots = packetOpenWindow.getSlotCount();
+
+            // Only look at standard chest sizes: 9x3 (27) or 9x6 (54)
+            if ((27 == slots || 54 == slots) && title instanceof final ChatComponentTranslation translation) {
+                final String key = translation.getKey();
+
+                if ("container.chest".equals(key) || "container.chestDouble".equals(key)) {
+                    Minecraft.getMinecraft().getNetHandler().getNetworkManager().sendPacket(new C0DPacketCloseWindow(packetOpenWindow.getWindowId()));
+                    return false;
+                }
             }
         }
         return true;
