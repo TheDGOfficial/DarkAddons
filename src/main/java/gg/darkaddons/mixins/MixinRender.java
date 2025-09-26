25158cde2454e2661689ed17925d0e4151e4c6fa
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2025-09-26 03:49:54 +0300

    mixin priority increase

--- src/main/java/gg/darkaddons/mixins/MixinRender.java
+++ src/main/java/gg/darkaddons/mixins/MixinRender.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Render.class, priority = 1_001)
+@Mixin(value = Render.class, priority = 1_002)
 final class MixinRender {
     private MixinRender() {
         super();
